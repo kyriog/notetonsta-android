664212a4b647966d9d9bcfcb3fe3cfb45faaa65e
Cyril Glapa <cyril.glapa@keuse.fr>
2012-04-03 15:03:00 +0200

    Creating project bases

---
+++ src/com/supinfo/notetonsta/android/ChooseCampusActivity.java
@@ -0,0 +1,13 @@
+package com.supinfo.notetonsta.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ChooseCampusActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.choosecampus);
+    }
+}
